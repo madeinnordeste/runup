2b57557fc78f3b054c4034800a283f594c6cc4d8
Luiz Alberto S. Ribeiro <madeinnordeste@gmail.com>
2018-01-23 00:24:24 -0300

    vishh

--- runup.sh
+++ runup.sh
@@ -13,17 +13,17 @@ case $1 in
         git submodule init
         git submodule update
         cd laradock/
-        cp env-example .env
-        docker-compose up -d workspace
-        docker-compose exec --user=laradock workspace composer create-project laravel/laravel $SOURCESDIR --prefer-dist
+        #cp env-example .env
+        #docker-compose up -d workspace
+        #docker-compose exec --user=laradock workspace composer create-project laravel/laravel $SOURCESDIR --prefer-dist
         docker-compose exec workspace chmod 777 -R bootstrap/cache/
         docker-compose exec workspace chmod 777 -R storage/
-        cp -Rf $SOURCESDIR/* ./
-        rm -Rf $SOURCESDIR/
+        #cp -Rf $SOURCESDIR/* ./
+        #rm -Rf $SOURCESDIR/
 		;;
-	laravel)
-		echo "laravel"
-		break
+	reclone)
+		echo "Re-clone gitrepo"
+		git clone git@github.com:madeinnordeste/runup.git .
 		;;
 	*)
 		echo "Sorry :( I don't understand"
